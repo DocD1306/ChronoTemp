d1d3b635303bf0691c8823d77d095279be4b5573
Diego <diegofjda2@gmail.com>
2025-08-06 10:15:39 +0200

    Updated DayTemperature. Some attributes were deleted or added.

--- src/main/java/com/chronotemp/ChronoTemp/model/DayTemperature.java
+++ src/main/java/com/chronotemp/ChronoTemp/model/DayTemperature.java
@@ -1,17 +1,15 @@
 package com.chronotemp.ChronoTemp.model;
 
+import java.time.LocalDate;
+
 /**
  * This class stores all the information related to a specific day of the year and a place.
  */
 public class DayTemperature {
 
-    // Properties
-    private String cityName;
-    private String country;
-    private double latitude;
-    private double longitude;
     private double maxTemperature;
     private double minTemperature;
+    private LocalDate date;
 
 
 }
