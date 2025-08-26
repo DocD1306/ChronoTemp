0af16ca6e7f8df9a70d45ff9a9eda1df9a4c37c2
diego <diegofjda2@gmail.com>
2025-08-26 21:24:56 +0200

    Created DayTemperatureRepository for the connection and operations with the database

---
+++ src/main/java/com/chronotemp/ChronoTemp/repository/DayTemperatureRepository.java
@@ -0,0 +1,18 @@
+package com.chronotemp.ChronoTemp.repository;
+
+import com.chronotemp.ChronoTemp.model.DayTemperature;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+/**
+ * This interface is used to connect to the database and provides all the functionalities for the most common
+ * operations or queries
+ */
+public interface DayTemperatureRepository extends JpaRepository<DayTemperature, Long> {
+
+    // This like the name describes, retrieves a DayTemperature for the input passed.
+    Optional<DayTemperature> findByCiudadAndPaisAndFecha(String ciudad, String pais, LocalDate fecha);
+
+}
