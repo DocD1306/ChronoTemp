25457485089ef56733455037b7f1be097af8f3b2
diego <diegofjda2@gmail.com>
2025-08-26 21:19:25 +0200

    Defined the properties of the DayTemperature model class

--- src/main/java/com/chronotemp/ChronoTemp/model/DayTemperature.java
+++ src/main/java/com/chronotemp/ChronoTemp/model/DayTemperature.java
@@ -1,15 +1,36 @@
 package com.chronotemp.ChronoTemp.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * This class stores all the information related to a specific day of the year and a place.
  */
+@Entity
+@Table(name = "DayTemperature")
+@Data
 public class DayTemperature {
 
-    private double maxTemperature;
-    private double minTemperature;
-    private LocalDate date;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String ciudad;
+
+    private String pais;
+
+    private double latitud;
+    private double longitud;
+
+    private LocalDate fecha;
+
+    private Double temperaturaMax;
+    private Double temperaturaMin;
+    private Double temperaturaMedia;
+
+    private LocalDateTime ultimaActualizacion;
 
 }
